976dd366155d901690cf0af0422dd99cd1b28e20
Shuhuan Xi <shuhuanx@yeah.net>
2021-02-10 20:24:31 +0800

    update

--- app/src/main/java/xyz/imaginarycrisis/wanandroidapp/RegisterActivity.java
+++ app/src/main/java/xyz/imaginarycrisis/wanandroidapp/RegisterActivity.java
@@ -2,10 +2,14 @@ package xyz.imaginarycrisis.wanandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -13,11 +17,41 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initTopBarViews("注册",R.id.register_tvb_layout);
     }
 
     static void activityStart(Context context){
         Intent intent = new Intent(context,RegisterActivity.class);
         context.startActivity(intent);
 
+    }
+
+    private void initTopBarViews(String title, int tarLayoutId){
+        Activity thisActivity = this;
+        TextView titleTv;
+        titleTv = findViewById(tarLayoutId).findViewById(R.id.top_view_bar_title);
+        titleTv.setText(title);
+        StatusBarUtils.setWindowStatusBarColor(this,R.color.orange_for_top_view_bar);
+        Button btnBack = findViewById(tarLayoutId).findViewById(R.id.top_view_bar_back_button);
+        TextView tvRefresh = findViewById(tarLayoutId).findViewById(R.id.top_view_bar_refresh);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thisActivity.finish();
+            }
+        });
+        tvRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        //刷新方法
+                    }
+                }.run();
+                Toast.makeText(thisActivity,"已刷新",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
